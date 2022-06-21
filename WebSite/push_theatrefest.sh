f77405a126c1a0e5044bfd4bc1631202df51ae22
Graham Cockell <graham.cockell@outlook.com>
2022-06-21 09:10:35 +0100

    Remove old Azure related files

--- WebSite/push_theatrefest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rsync -av --delete --exclude-from=push-exclude.txt . graham@theatrefestvm.ukwest.cloudapp.azure.com:/home/graham/theatrefest/website/
-rsync -av --delete ./django_static/* graham@theatrefestvm.ukwest.cloudapp.azure.com:/home/graham/theatrefest/website/static
-
